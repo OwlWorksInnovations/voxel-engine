e16fef1e91c568fce90c44e882115d3bbd0db9b2
OwlWorksInnovations <juanpistoris01@gmail.com>
2026-02-25 13:45:49 +0200

    Now this looks more like a traditional opengl tutorial. I finally got a triangle with per vertex color attributes

--- voxel-engine/main.cpp
+++ voxel-engine/main.cpp
@@ -4,17 +4,20 @@
 
 const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 ourColor;\n"
 	"void main()\n"
 	"{\n"
-	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"   gl_Position = vec4(aPos, 1.0);\n"
+	"   ourColor = aColor;\n"
 	"}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
-	"uniform vec4 ourColor;\n"
+	"in vec3 ourColor;\n"
 	"void main()\n"
 	"{\n"
-	"	FragColor = ourColor;"
+	"	FragColor = vec4(ourColor, 1.0);"
 	"}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -101,22 +104,14 @@ int main() {
 	glDeleteShader(vertexShader);
 
 	// Triangle
-	float squareVertices[] = {
-		-0.25f,  0.25f, 0.0f,  // top right
-		-0.25f, -0.25f, 0.0f,  // bottom right
-		-0.75f,  0.25f, 0.0f,  // top left 
-		-0.75f, -0.25f, 0.0f,  // bottom left
-		0.75f, 0.25f, 0.0f,  // top right
-		0.75f, -0.25f, 0.0f,  // bottom right
-		0.25f, 0.25f, 0.0f,  // top left 
-		0.25f, -0.25f, 0.0f,
+	float triangleVertices[] = {
+		 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // Top
+		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // Right bottom
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // Left bottom
 	};
 
-	unsigned int squareIndices[] = {
-		0, 1, 3,
-		0, 2, 3,
-		4, 5, 7,
-		4, 6, 7,
+	unsigned int triangleIndices[] = {
+		0, 1, 2,
 	};
 
 	// Set up VBO (Vertex Buffer Object), VAO (Vertex Array Object), EBO (Element Buffer Object)
@@ -128,10 +123,12 @@ int main() {
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO );
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(squareVertices), squareVertices, GL_STATIC_DRAW);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(squareIndices), squareIndices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangleIndices), triangleIndices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// Unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -148,14 +145,8 @@ int main() {
 		// Draw triangle
 		glUseProgram(shaderProgram);
 
-		// Set the color
-		float timeValue = glfwGetTime();
-		float greenValue = sin(timeValue) / 2.0f + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		// Rendering ends here

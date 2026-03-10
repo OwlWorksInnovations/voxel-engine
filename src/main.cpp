0c888bdeafe3ae6dd2e75c0c726c7c9d04ad7a66
OwlWorksInnovations <juanpistoris01@gmail.com>
2026-03-10 14:07:16 +0200

    Going a bit back. I got a lot of scope creep so lets first make a game then the engine

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,6 @@
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/fwd.hpp"
 #include "glm/trigonometric.hpp"
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl3.h"
 #include "stb_image.h"
 #include <GLFW/glfw3.h>
 #include <cstdlib>
@@ -26,28 +23,22 @@ float lastFrame = 0.0f;
 int screenWidth = 1920;
 int screenHeight = 1080;
 
-// Entity
-struct Entity {
-  std::string name;
-  int id;
-};
-std::vector<Entity> entities;
-int selectedEntity = -1;
+// ECS Registry
 Registry registry;
 
 // Camera
-glm::vec3 cameraPos = glm::vec3(0.0f, 10.0f, 17.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraPos = glm::vec3(8.0f, 20.0f, 25.0f);
+glm::vec3 cameraFront = glm::normalize(glm::vec3(0.0f, -0.5f, -1.0f));
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 float yaw = -90.0f;
-float pitch = 0.0f;
+float pitch = -30.0f;
 float lastX = 400.0f;
 float lastY = 300.0f;
 bool firstMouse = true;
 
 // Other
 bool wireframeMode = false;
-bool sceneViewFocused = false;
+bool mouseCaptured = true;
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   (void)window;
@@ -58,7 +49,7 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
 
 void mouse_callback(GLFWwindow *window, double xposIn, double yposIn) {
   (void)window;
-  if (!sceneViewFocused)
+  if (!mouseCaptured)
     return;
 
   float xpos = static_cast<float>(xposIn);
@@ -94,23 +85,22 @@ void mouse_callback(GLFWwindow *window, double xposIn, double yposIn) {
   cameraFront = glm::normalize(direction);
 }
 
-void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
-  (void)window;
-  (void)xoffset;
-  (void)yoffset;
-  // reserved for zoom later
-}
-
 void process_input(GLFWwindow *window) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-    sceneViewFocused = false;
+    mouseCaptured = false;
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
   }
 
-  if (!sceneViewFocused)
+  if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+    mouseCaptured = true;
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    firstMouse = true;
+  }
+
+  if (!mouseCaptured)
     return;
 
-  float cameraSpeed = 2.5f * deltaTime;
+  float cameraSpeed = 5.0f * deltaTime;
   if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
     cameraPos += cameraSpeed * cameraFront;
   if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
@@ -148,58 +138,25 @@ int main() {
   glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
   glfwSetCursorPosCallback(window, mouse_callback);
-  glfwSetScrollCallback(window, scroll_callback);
-  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD!" << std::endl;
     return -1;
   }
-  // Set window size
-  glViewport(0, 0, screenWidth, screenHeight);
+  
   glEnable(GL_DEPTH_TEST);
 
-  // Load shaders into shader class
+  // Load shaders
   Shader ourShader("shader.vs", "shader.fs");
 
-  // Set up ImGui
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGuiIO &io = ImGui::GetIO();
-  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-  ImGui_ImplGlfw_InitForOpenGL(window, true);
-  ImGui_ImplOpenGL3_Init("#version 330");
-
-  // Create framebuffer
-  unsigned int FBO;
-  glGenFramebuffers(1, &FBO);
-  glBindFramebuffer(GL_FRAMEBUFFER, FBO);
-
-  // Create texture to render into
-  unsigned int renderTexture;
-  glGenTextures(1, &renderTexture);
-  glBindTexture(GL_TEXTURE_2D, renderTexture);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, screenWidth, screenHeight, 0, GL_RGB,
-               GL_UNSIGNED_BYTE, NULL);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
-                         renderTexture, 0);
-
-  // Create renderbuffer for depth
-  unsigned int RBO;
-  glGenRenderbuffers(1, &RBO);
-  glBindRenderbuffer(GL_RENDERBUFFER, RBO);
-  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, screenWidth,
-                        screenHeight);
-  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT,
-                            GL_RENDERBUFFER, RBO);
-
-  glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-  // Load and create texture
+  // Load and create textures
   Texture containerTexture("assets/textures/container.jpg", GL_RGB, 0,
                            GL_REPEAT, GL_REPEAT, GL_NEAREST_MIPMAP_LINEAR,
                            GL_LINEAR);
+  Texture awesomefaceTexture("assets/textures/awesomeface.png", GL_RGBA, 0,
+                             GL_REPEAT, GL_REPEAT, GL_NEAREST_MIPMAP_LINEAR,
+                             GL_LINEAR);
+
   ourShader.use();
   ourShader.setInt("texture1", 0);
 
@@ -207,44 +164,44 @@ int main() {
   Mesh cubeMesh;
   cubeMesh.create(DefaultMeshes::cubeVertices, DefaultMeshes::cubeIndices);
 
-  // Perlin noise
+  // Perlin noise terrain
   ChunkManager chunkManager;
   PerlinNoise perlin;
+  float maxHeight = 15.0f;
   perlin.setMapSize(16, 16, 16.0f);
   perlin.generatePerlinNoise();
-  perlin.generateVoxelData(chunkManager.chunk, perlin.sampledValues, 15);
+  perlin.generateVoxelData(chunkManager.chunk, perlin.sampledValues, maxHeight);
   chunkManager.createChunk();
-  perlin.generateImage("perlinnoise.png");
+
+  // Create cube entity on top of terrain at (8, 8)
+  int cubeX = 8;
+  int cubeZ = 8;
+  int terrainHeight = (perlin.sampledValues[cubeX * 16 + cubeZ] + 1.0f) / 2.0f * maxHeight;
+  
+  int entityId = registry.createEntity();
+  registry.addComponent<Transform>(entityId);
+  Transform* transform = registry.getTransform(entityId);
+  transform->position = glm::vec3(cubeX, terrainHeight + 1.5f, cubeZ);
+  transform->scale = glm::vec3(1.0f, 1.0f, 1.0f);
+  
+  registry.addMeshRenderer(entityId, cubeMesh.VAO, awesomefaceTexture.textureID);
 
   // Render loop
   while (!glfwWindowShouldClose(window)) {
-
-    // deltaTime
     float currentFrame = glfwGetTime();
     deltaTime = currentFrame - lastFrame;
     lastFrame = currentFrame;
 
-    // Setup
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-    ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport());
-
     process_input(window);
-    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    if (wireframeMode) {
+
+    if (wireframeMode)
       glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    } else {
+    else
       glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    }
 
-    // Bind framebuffer and render scene into it
-    glBindFramebuffer(GL_FRAMEBUFFER, FBO);
-    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // Freecam
     ourShader.use();
     glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
     glm::mat4 projection = glm::perspective(
@@ -253,11 +210,9 @@ int main() {
     ourShader.setMat4("view", view);
     ourShader.setMat4("projection", projection);
 
-    // Set textures
+    // Draw chunks
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, containerTexture.textureID);
-
-    // Draw chunks
     for (Chunk &chunk : chunkManager.chunks) {
       glm::mat4 model = glm::mat4(1.0f);
       ourShader.setMat4("model", model);
@@ -267,19 +222,15 @@ int main() {
 
     // Draw entities
     for (MeshRenderer &mr : registry.meshRenderers) {
-      Transform *transform = registry.getTransform(mr.entityID);
-      if (!transform)
-        continue;
+      Transform *t = registry.getTransform(mr.entityID);
+      if (!t) continue;
 
       glm::mat4 model = glm::mat4(1.0f);
-      model = glm::translate(model, transform->position);
-      model = glm::rotate(model, glm::radians(transform->rotation.x),
-                          glm::vec3(1, 0, 0));
-      model = glm::rotate(model, glm::radians(transform->rotation.y),
-                          glm::vec3(0, 1, 0));
-      model = glm::rotate(model, glm::radians(transform->rotation.z),
-                          glm::vec3(0, 0, 1));
-      model = glm::scale(model, transform->scale);
+      model = glm::translate(model, t->position);
+      model = glm::rotate(model, glm::radians(t->rotation.x), glm::vec3(1, 0, 0));
+      model = glm::rotate(model, glm::radians(t->rotation.y), glm::vec3(0, 1, 0));
+      model = glm::rotate(model, glm::radians(t->rotation.z), glm::vec3(0, 0, 1));
+      model = glm::scale(model, t->scale);
       ourShader.setMat4("model", model);
 
       glBindTexture(GL_TEXTURE_2D, mr.textureID);
@@ -287,117 +238,11 @@ int main() {
       glDrawElements(GL_TRIANGLES, cubeMesh.indexCount, GL_UNSIGNED_INT, 0);
     }
 
-    // Unbind framebuffer
-    glBindVertexArray(0);
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-    // Hierarchy panel
-    ImGui::Begin("Hierarchy");
-    if (ImGui::BeginPopupContextWindow("HierarchyContext")) {
-      if (ImGui::MenuItem("Create Entity")) {
-        Entity e;
-        int id = registry.createEntity();
-        e.name = "Entity " + std::to_string(id);
-        e.id = id;
-        entities.push_back(e);
-        registry.addComponent<Transform>(id);
-      }
-      ImGui::EndPopup();
-    }
-    for (int i = 0; i < (int)chunkManager.chunks.size(); i++) {
-      ImGui::Text("Chunk %d", i);
-    }
-    for (int i = 0; i < (int)entities.size(); i++) {
-      bool selected = (selectedEntity == i);
-      if (ImGui::Selectable(entities[i].name.c_str(), selected))
-        selectedEntity = i;
-    }
-    ImGui::End();
-
-    // Scene view panel (UV flipped to correct OpenGL/ImGui Y-axis mismatch)
-    ImGui::Begin("Scene");
-    if (ImGui::IsWindowHovered() && ImGui::IsMouseClicked(0)) {
-      sceneViewFocused = true;
-      glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-      firstMouse = true;
-    }
-    ImGui::Image((ImTextureID)(intptr_t)renderTexture,
-                 ImGui::GetContentRegionAvail(), ImVec2(0, 1), ImVec2(1, 0));
-    ImGui::End();
-
-    // Properties panel
-    ImGui::Begin("Properties");
-    if (selectedEntity >= 0 && selectedEntity < (int)entities.size()) {
-      Entity &e = entities[selectedEntity];
-
-      // Name field
-      char nameBuf[128];
-      strncpy(nameBuf, e.name.c_str(), sizeof(nameBuf));
-      if (ImGui::InputText("Name", nameBuf, sizeof(nameBuf)))
-        e.name = nameBuf;
-
-      ImGui::Separator();
-
-      // Transform component
-      Transform *transform = registry.getTransform(e.id);
-      if (transform) {
-        if (ImGui::CollapsingHeader("Transform",
-                                    ImGuiTreeNodeFlags_DefaultOpen)) {
-          ImGui::DragFloat3("Position", &transform->position.x, 0.1f);
-          ImGui::DragFloat3("Rotation", &transform->rotation.x, 0.1f);
-          ImGui::DragFloat3("Scale", &transform->scale.x, 0.1f);
-        }
-      }
-
-      // MeshRenderer component
-      MeshRenderer *mesh = nullptr;
-      for (MeshRenderer &mr : registry.meshRenderers)
-        if (mr.entityID == e.id) {
-          mesh = &mr;
-          break;
-        }
-      if (mesh) {
-        if (ImGui::CollapsingHeader("Mesh Renderer",
-                                    ImGuiTreeNodeFlags_DefaultOpen)) {
-          ImGui::Text("VAO: %u", mesh->VAO);
-          ImGui::Text("Texture ID: %u", mesh->textureID);
-        }
-      }
-
-      ImGui::Separator();
-
-      // Add component button
-      if (ImGui::Button("Add Component"))
-        ImGui::OpenPopup("AddComponent");
-      if (ImGui::BeginPopup("AddComponent")) {
-        if (!transform && ImGui::MenuItem("Transform"))
-          registry.addComponent<Transform>(e.id);
-        if (!mesh && ImGui::MenuItem("Mesh Renderer"))
-          registry.addMeshRenderer(e.id, cubeMesh.VAO,
-                                   containerTexture.textureID);
-        ImGui::EndPopup();
-      }
-    } else {
-      ImGui::Text("No entity selected");
-    }
-    ImGui::End();
-
-    // ImGui rendering
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
-  // Cleanup
-  ImGui_ImplOpenGL3_Shutdown();
-  ImGui_ImplGlfw_Shutdown();
-  ImGui::DestroyContext();
   cubeMesh.destroy();
-  glDeleteFramebuffers(1, &FBO);
-  glDeleteRenderbuffers(1, &RBO);
-  glDeleteTextures(1, &renderTexture);
   glfwTerminate();
   return 0;
 }

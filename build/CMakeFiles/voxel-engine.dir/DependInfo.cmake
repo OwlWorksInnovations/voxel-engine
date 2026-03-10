512d8c2e5f08ec47a31077635abab91816c5cde6
OwlWorksInnovations <juanpistoris01@gmail.com>
2026-03-10 17:17:14 +0200

    Engine rewrite on its way

---
+++ build/CMakeFiles/voxel-engine.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/juanpistorius/Documents/development/cpp/voxel-engine/src/main.cpp" "CMakeFiles/voxel-engine.dir/src/main.cpp.o" "gcc" "CMakeFiles/voxel-engine.dir/src/main.cpp.o.d"
+  "/home/juanpistorius/Documents/development/cpp/voxel-engine/src/stb.cpp" "CMakeFiles/voxel-engine.dir/src/stb.cpp.o" "gcc" "CMakeFiles/voxel-engine.dir/src/stb.cpp.o.d"
+  "" "/home/juanpistorius/Documents/development/cpp/voxel-engine/dist/voxel-engine" "gcc" "CMakeFiles/voxel-engine.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
